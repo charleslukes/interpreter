e7a3c69df3bde40d6262094299494cc46902a0f9
charleslukes <charleslukes28@gmail.com>
2023-07-28 17:28:18 +0100

    add asci constants and keywords

---
+++ src/token.rs
@@ -0,0 +1,31 @@
+pub const ILLEGAL: &'static str = "ILLEGAL";
+pub const EOF: &'static str = "EOF";
+pub const IDENT: &'static str = "IDENT";
+pub const _INT: &'static str = "INT";
+
+pub const ASSIGN: &'static str = "=";
+pub const PLUS: &'static str = "+";
+pub const MINUS: &'static str = "-";
+pub const BANG: &'static str = "!";
+pub const ASTERISK: &'static str = "*";
+pub const SLASH: &'static str = "/";
+
+pub const LT: &'static str = "<";
+pub const GT: &'static str = ">";
+pub const EQ: &'static str = "==";
+pub const NOT_EQ: &'static str = "!=";
+
+pub const COMMA: &'static str = ",";
+pub const SEMICOLON: &'static str = ";";
+pub const LPAREN: &'static str = "(";
+pub const RPAREN: &'static str = ")";
+pub const LBRACE: &'static str = "{";
+pub const RBRACE: &'static str = "}";
+
+pub const FUNCTION: &'static str = "FUNCTION";
+pub const LET: &'static str = "LET";
+pub const TRUE: &'static str = "TRUE";
+pub const FALSE: &'static str = "FALSE";
+pub const IF: &'static str = "IF";
+pub const ELSE: &'static str = "ELSE";
+pub const RETURN: &'static str = "RETURN";
